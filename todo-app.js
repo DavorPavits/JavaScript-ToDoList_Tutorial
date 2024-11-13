64695c372eb77f76c22115fb0228012da760359a
DavorPavits <163551802+DavorPavits@users.noreply.github.com>
2024-11-13 19:27:09 +0100

    Completed

--- todo-app.js
+++ todo-app.js
@@ -1,5 +1,70 @@
 const form = document.getElementById("form");
-
 const input = document.getElementById("input");
+const todoUL  = document.getElementById("todos");
 
-const todoUL  =document.getElementById("todos");
+
+const todos  = JSON.parse(localStorage.getItem("todos"));
+
+if(todos){
+    todos.forEach((todo) => {
+        addTodo(todo);
+    });
+}
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    addTodo();
+});
+
+function addTodo(todo){
+    let todoText = input.value;
+
+    if(todo){
+        todoText = todo.text;
+    }
+
+    if(todoText){
+        const todo1 = document.createElement("li");
+        if(todo && todo.completed){
+            todo1.classList.add("completed");
+        }
+
+        todo1.innerText = todoText;
+
+        todo1.addEventListener("click", ()=>{
+            todo1.classList.toggle("completed");
+
+            updateLS();
+        });
+
+        todo1.addEventListener("contextmenu", (e)=>{
+            e.preventDefault();
+
+            todo1.remove();
+
+            updateLS();
+        });
+
+        todoUL.appendChild(todo1);
+
+        input.Value = "";
+
+        updateLS();
+    }
+}
+
+function updateLS(){
+    const todo1 = document.querySelectorAll("li");
+
+    const todos = [];
+
+    todo1.forEach((todo1) => {
+        todos.push({
+            text: todo1.innerText,
+            completed: todo1.classList.contains("completed"),
+        });
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
